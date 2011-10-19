855437e348bf9f92e6a6b7a3d8579f29b10d629b
Nickolay <devmarkup@gmail.com>
2011-10-19 11:01:10 +0300

    move methods

--- main.rb
+++ main.rb
@@ -23,6 +23,23 @@ class Main < Sinatra::Base
     haml template, :locals => {:active => template}.merge(options)
   end
 
+  def captcha_pass?
+    session = params[:captcha_session].to_i
+    answer = params[:captcha_answer].gsub(/\W/, '')
+    open("http://captchator.com/captcha/check_answer/#{session}/#{answer}").read.to_i.nonzero? rescue false
+  end
+
+  def captcha_session
+    @captcha_session ||= rand(9000) + 1000
+  end
+
+  def captcha_tags
+    {
+      :session => captcha_session,
+      :image => "http://captchator.com/captcha/image/#{captcha_session}"
+    }
+  end
+
   get '/css/screen.css' do
    content_type :css
    sass :stylesheet # overridden
@@ -66,21 +83,4 @@ class Main < Sinatra::Base
   end
 
   helpers Sinatra::ContentFor
-
-  def captcha_pass?
-    session = params[:captcha_session].to_i
-    answer = params[:captcha_answer].gsub(/\W/, '')
-    open("http://captchator.com/captcha/check_answer/#{session}/#{answer}").read.to_i.nonzero? rescue false
-  end
-
-  def captcha_session
-    @captcha_session ||= rand(9000) + 1000
-  end
-
-  def captcha_tags
-    {
-      :session => captcha_session,
-      :image => "http://captchator.com/captcha/image/#{captcha_session}"
-    }
-  end
 end
